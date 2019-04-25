da5f514b03b643bc9eea4eaa20bdc7e41f02707b
Todd Radel <todd@hashicorp.com>
2019-04-24 23:17:28 -0400

    Add retry counter

--- files/supervisord/proxy-8877.sh
+++ files/supervisord/proxy-8877.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-pod_id=`/usr/bin/kubectl --kubeconfig="/root/.kube/config" get pods -l service=ambassador -o jsonpath='{.items[0].metadata.name}'`
+pod_id=`/usr/bin/kubectl --kubeconfig="/root/.kube/config" get pod -l service=ambassador -o jsonpath='{.items[0].metadata.name}'`
 /usr/bin/kubectl --kubeconfig="/root/.kube/config" port-forward ${pod_id} 8878:8877
 
