5b5307e3e16dad5b39e2cc663801c4a9329389e4
Todd Radel <todd@hashicorp.com>
2019-04-24 23:17:14 -0400

    Try weave instead of flannel

--- run/start.sh
+++ run/start.sh
@@ -5,7 +5,7 @@ export DIND_K8S_VERSION=${K8S_VERSION}
 export SKIP_DASHBOARD=true 
 export SKIP_SNAPSHOT=true
 export NUM_NODES=3
-export CNI_PLUGIN=flannel
+export CNI_PLUGIN=weave
 
 cd $HOME/dind
 ./dind-cluster.sh restore
